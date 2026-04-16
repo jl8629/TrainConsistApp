57413770698c71dc058308fe60fcd2bb55dc9863
jl8629 <jl8629@srmist.edu.in>
2026-04-16 10:19:14 +0530

    Jasmitha added UC17

--- App/src/TrainConsistApp.java
+++ App/src/TrainConsistApp.java
@@ -1,34 +1,9 @@
-import java.util.*;
-import java.util.stream.*;
-
-class Bogie {
-    int id;
-    int capacity;
-
-    Bogie(int id, int capacity) {
-        this.id = id;
-        this.capacity = capacity;
-    }
-
-    public String toString() {
-        return "Bogie ID: " + id + ", Capacity: " + capacity;
-    }
-}
+import java.util.Arrays;
 
 public class TrainConsistApp {
     public static void main(String[] args) {
-        List<Bogie> bogies = Arrays.asList(
-            new Bogie(1, 50),
-            new Bogie(2, 75),
-            new Bogie(3, 60),
-            new Bogie(4, 80),
-            new Bogie(5, 45)
-        );
-
-        List<Bogie> filteredBogies = bogies.stream()
-                                           .filter(b -> b.capacity > 60)
-                                           .toList();
-
-        filteredBogies.forEach(System.out::println);
+        String[] bogieTypes = {"Sleeper", "General", "AC Chair Car", "First Class", "Second Class"};
+        Arrays.sort(bogieTypes);
+        System.out.println("Sorted Bogie Types: " + Arrays.toString(bogieTypes));
     }
 }
